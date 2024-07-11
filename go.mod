ea9891c200ba9734c376085e4494bb7980c5cbc5
Khalad0007 <www.arifhossain49919@gmail.com>
2024-07-11 23:57:16 +0600

    Trash

--- go.mod
+++ go.mod
@@ -10,8 +10,10 @@ require (
 	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pusher/pusher-http-go/v5 v5.1.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.55.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/crypto v0.24.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,21 +1,39 @@
 package main
 
 import (
-    "log"
+	"log"
 
-    "github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
+	"github.com/pusher/pusher-http-go/v5"
 )
 
 func main() {
-    // Initialize a new Fiber app
-    app := fiber.New()
+	// Initialize a new Fiber app
+	app := fiber.New()
 
-    // Define a route for the GET method on the root path '/'
-    app.Get("/", func(c fiber.Ctx) error {
-        // Send a string response to the client
-        return c.SendString("Hello, World 👋!")
-    })
+	app.Use(cors.New())
+	pusherClient := pusher.Client{
+		AppID:   "1832921",
+		Key:     "1817dc92d62fbdbbe132",
+		Secret:  "f722d129140a7e64a942",
+		Cluster: "ap2",
+		Secure:  true,
+	}
+	// Define a route for the GET method on the root path '/'
+	app.Post("/api/messages", func(c fiber.Ctx) error {
 
-    // Start the server on port 3000
-    log.Fatal(app.Listen(":3000"))
+		var data map[string]string
+
+		if err := c.BodyParser(&data); err != nil {
+			return err
+		}
+
+		pusherClient.Trigger("chat", "message", data)
+		// Send a string response to the client
+		return c.JSON([]string{})
+	})
+
+	// Start the server on port 3000
+	log.Fatal(app.Listen(":3000"))
 }
